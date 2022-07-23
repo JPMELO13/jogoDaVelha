f333a287d49511538e7e2f500aec6d2c2ca8c9bf
JPMELO13 <jpgeninho@gmail.com>
2022-07-23 17:59:12 -0300

    logica inicial funcionando

--- script.js
+++ script.js
@@ -1,45 +1,83 @@
-
-
 var velha = [[0,0,0],
              [0,0,0],
              [0,0,0]]
 
-
-
-
-
-
+var jogador = 1;
+var ganhador = 0;
 
 // Sera executado apos o carregamento da pagina
+
+function mudarJogador(){
+  jogador = jogador * -1;
+}
+
+function verificarVitoria(){
+  if (((velha[0][0] + velha[1][1] + velha[2][2])==3)||((velha[0][2] + velha[1][1] + velha[2][0])==3)){
+    ganhador = 1;
+  }
+  else if((velha[0][0] + velha[1][1] + velha[2][2]==-3)||(velha[0][2] + velha[1][1] + velha[2][0]==-3)){
+    ganhador = -1;
+  }
+  else{
+    for(let cont=0; cont<3; cont++){
+
+      if(velha[cont][0] + velha[cont][1] + velha[cont][2] == '3'){
+        ganhador = 1
+      }
+      else if(velha[cont][0] + velha[cont][1] + velha[cont][2] == "-3"){
+        ganhador = -1
+      }
+  
+      if(velha[0][cont] + velha[1][cont] + velha[2][cont] == '3'){
+        ganhador = 1
+      }
+      else if(velha[0][cont] + velha[1][cont] + velha[2][cont] == "-3"){
+        ganhador = -1
+      }
+    }
+  }
+  if(ganhador==1){    
+    document.getElementById("titulo").innerText = "GANHADOR 1";
+    console.log('ganhou 1')
+  }
+  else if(ganhador ==-1){
+    document.getElementById("titulo").innerText = "GANHADOR -1";
+    console.log('ganhou -1')
+  }
+
+  
+
+}
+
 window.onload = function() {
     // vamos pegar todos os botoes:
     var botoes = document.querySelectorAll(".botoes");
-       
-    for(var i=0;i<botoes.length;i++) {
+    
+    for(let i=0;i<botoes.length;i++) {
       
       var botao = botoes[i];
-      
-       
-      console.log(botao.id);
       
       // O jeito correto e padronizado de incluir eventos no ECMAScript
       // (Javascript) eh com addEventListener:
       botao.addEventListener("click", function(){
-        if (this.value=="0"){
-          this.value = parseInt(this.value)+1;
-          this.innerText = "1";
+        if (this.value=="0" && ganhador==0){
+          this.value = parseInt(this.value)+ jogador;
+          this.innerText = jogador;
           this.style.border = "0.5em solid black"
           idMod=parseInt(this.id)
           if (idMod<3){
-            velha[0][idMod] = this.value 
+            velha[0][idMod] = parseInt(this.value)
           }
           else if (idMod<6){
-            velha[1][idMod-3] = this.value 
+            velha[1][idMod-3] = parseInt(this.value) 
           }
           else{
-            velha[2][idMod-6] = this.value 
+            velha[2][idMod-6] = parseInt(this.value) 
           }
         }
+        console.log(velha);
+        mudarJogador();
+        verificarVitoria();
       });
     }
  };
